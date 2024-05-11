6d1e641e0e51dfb50ccb2a61c058df54cc267de4
blackPeppper <spices.digital@gmail.com>
2024-05-11 06:02:02 +0300

    dynamic route try

---
+++ src/content/config.ts
@@ -0,0 +1,10 @@
+import { defineCollection, z } from "astro:content";
+
+const docs = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
+export const collections = { docs };
